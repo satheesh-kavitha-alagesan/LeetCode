fe07f125ba04e2dac3b13d82d3bf8e2cd7297848
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-02-07 15:37:21 +0530

    Time: 532 ms (65.48%) | Memory: 29.6 MB (71.71%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,17 +1,30 @@
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         start = 0
+#         end = len(height)
+#         maxvol = 0
+#         while True:
+#             leng = end - (start +1)
+#             if start == end - 1:
+#                 break
+#             elif height[start] > height[end - 1]:
+#                 curvol = height[end - 1] * leng
+#                 end -= 1
+#             else:
+#                 curvol = height[start] * leng
+#                 start  += 1
+#             maxvol = max(maxvol, curvol)
+#         return maxvol
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        start = 0
-        end = len(height)
-        maxvol = 0
-        while True:
-            leng = end - (start +1)
-            if start == end - 1:
-                break
-            elif height[start] > height[end - 1]:
-                curvol = height[end - 1] * leng
-                end -= 1
+        left = 0
+        right = len(height) - 1
+        maxArea = 0
+        while left < right:
+            maxArea = max(maxArea, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
             else:
-                curvol = height[start] * leng
-                start  += 1
-            maxvol = max(maxvol, curvol)
-        return maxvol
+                right -= 1
+        return maxArea
