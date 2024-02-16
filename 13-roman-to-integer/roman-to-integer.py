827f94988ac0fefb49c0ae5a4ea03171393c02e5
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-02-16 09:14:54 +0530

    Time: 36 ms (96.12%) | Memory: 16.6 MB (81.17%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,18 +1,27 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        m = {"I" : 1, "IV" : 4, "V" : 5, "IX" : 9, "X" : 10, "XL" : 40, "L" : 50, "XC" : 90 , "C" : 100, "CD" : 400, "D" : 500, "CM" : 900, "M" : 1000}
-        passFlag = False
-        num = 0
-        for ind in range(len(s)):
-            if passFlag:
-                passFlag = False
-                continue
-            if ind+1 < len(s):
-                if (s[ind] + s[ind+1]) in m.keys():
-                    num += m[s[ind] + s[ind+1]]
-                    passFlag = True
-                else:
-                    num += m[s[ind]]
+        # m = {"I" : 1, "IV" : 4, "V" : 5, "IX" : 9, "X" : 10, "XL" : 40, "L" : 50, "XC" : 90 , "C" : 100, "CD" : 400, "D" : 500, "CM" : 900, "M" : 1000}
+        # passFlag = False
+        # num = 0
+        # for ind in range(len(s)):
+        #     if passFlag:
+        #         passFlag = False
+        #         continue
+        #     if ind+1 < len(s):
+        #         if (s[ind] + s[ind+1]) in m.keys():
+        #             num += m[s[ind] + s[ind+1]]
+        #             passFlag = True
+        #         else:
+        #             num += m[s[ind]]
+        #     else:
+        #         num += m[s[ind]]
+        # return num
+
+        roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        number=0
+        for i in range(len(s)-1):
+            if roman[s[i]] < roman[s[(i+1)]]:
+                number-=roman[s[i]]
             else:
-                num += m[s[ind]]
-        return num
+                number+=roman[s[i]]
+        return number+roman[s[-1]]
