fa0b80521db5c6c75216986d105d4bd9c0103358
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-02-17 14:02:23 +0530

    Time: 520 ms (94.66%) | Memory: 20.8 MB (58.42%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -12,34 +12,62 @@ class Solution:
         #                     if valList not in retList:
         #                         retList.append(valList)
         # return retList
+
+        # nums.sort()
+        # answer = []
+
+        # if len(nums) < 3:
+        #     return answer
+
+        # for i in range(len(nums)):
+        #     if nums[i] > 0:
+        #         break
+
+        #     if i > 0 and nums[i] == nums[i - 1]:
+        #         continue
+
+        #     low, high = i + 1, len(nums) - 1
+        #     while low < high:
+        #         s = nums[i] + nums[low] + nums[high]
+        #         if s > 0:
+        #             high -= 1
+        #         elif s < 0:
+        #             low += 1
+        #         else:
+        #             answer.append([nums[i], nums[low], nums[high]])
+        #             lastLowOccurrence, lastHighOccurrence = nums[low], nums[high]
+
+        #             while low < high and nums[low] == lastLowOccurrence:
+        #                 low += 1
+
+        #             while low < high and nums[high] == lastHighOccurrence:
+        #                 high -= 1
+
+        # return answer
         nums.sort()
-        answer = []
-        
-        if len(nums) < 3:
-            return answer
-        
-        for i in range(len(nums)):
-            if nums[i] > 0:
+        ret = []
+        print(nums)
+        for ind in range(len(nums)):
+            if nums[ind] > 0:
                 break
-            
-            if i > 0 and nums[i] == nums[i - 1]:
+            if ind > 0 and (nums[ind] == nums[ind-1]):
                 continue
-            
-            low, high = i + 1, len(nums) - 1
-            while low < high:
-                s = nums[i] + nums[low] + nums[high]
-                if s > 0:
-                    high -= 1
-                elif s < 0:
-                    low += 1
-                else:
-                    answer.append([nums[i], nums[low], nums[high]])
-                    lastLowOccurrence, lastHighOccurrence = nums[low], nums[high]
-                    
-                    while low < high and nums[low] == lastLowOccurrence:
-                        low += 1
-                    
-                    while low < high and nums[high] == lastHighOccurrence:
-                        high -= 1
-        
-        return answer
+            start = ind + 1
+            end = len(nums) - 1
+            remaining = 0 - nums[ind]
+            while start < end:
+                tot = nums[start] + nums[end]
+                if tot == remaining:
+                    ap = [nums[ind], nums[start], nums[end]]
+                    ret.append(ap)
+                    lastLowOccurrence, lastHighOccurrence = nums[start], nums[end]
+                    while start < end and nums[start] == lastLowOccurrence:
+                        start += 1
+
+                    while start < end and nums[end] == lastHighOccurrence:
+                        end -= 1
+                elif tot > remaining:
+                    end -= 1
+                elif tot < remaining:
+                    start += 1
+        return ret
