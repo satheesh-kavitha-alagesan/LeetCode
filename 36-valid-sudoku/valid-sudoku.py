1e8fd95740eb3d3a28b6c75b19b2a5140cbce370
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-02-24 11:51:52 +0530

    Time: 92 ms (63.06%) | Memory: 16.7 MB (55.42%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -27,21 +27,40 @@
 #                     return False
 #         return True
 
+# class Solution:
+#     def isValidSudoku(self, board: List[List[str]]) -> bool:
+#         seen = set()
+
+#         for i in range(9):
+#             for j in range(9):
+#                 num = board[i][j]
+#                 if num != '.':
+#                     row_key = (i, num)
+#                     col_key = (num, j)
+#                     box_key = (i // 3, j // 3, num)
+                    
+#                     if (row_key in seen) or (col_key in seen) or (box_key in seen):
+#                         return False
+
+#                     seen.update([row_key, col_key, box_key])
+
+#         return True
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        seen = set()
-
+        row = defaultdict(set)
+        col = defaultdict(set)
+        grid = defaultdict(set)
         for i in range(9):
             for j in range(9):
-                num = board[i][j]
-                if num != '.':
-                    row_key = (i, num)
-                    col_key = (num, j)
-                    box_key = (i // 3, j // 3, num)
-                    
-                    if (row_key in seen) or (col_key in seen) or (box_key in seen):
+                val = board[i][j]
+                if val.isalnum():
+                    k = i//3 * 3 + j//3
+                    if val in row[i] or val in col[j] or val in grid[k]:
                         return False
-
-                    seen.update([row_key, col_key, box_key])
+                    else:
+                        row[i].add(val)
+                        col[j].add(val)
+                        grid[k].add(val)
 
         return True
