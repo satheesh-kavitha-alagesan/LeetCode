fe85883d2c08ce1962d2d3190b10ec85223f3cd1
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-02-27 19:59:38 +0530

    Time: 40 ms (65.30%) | Memory: 16.8 MB (44.94%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,13 +1,29 @@
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         def rec(l1, l2):
+#             lr = []
+#             for l2e in l2:
+#                 for l1e in l1:
+#                     if l2e not in l1e:
+#                         lr.append(l1e + [l2e])
+#             return lr
+#         l1 = [[n] for n in nums]
+#         for _ in range(len(nums) -1):
+#             l1 = rec(l1, nums)
+#         return l1
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def rec(l1, l2):
-            lr = []
-            for l2e in l2:
-                for l1e in l1:
-                    if l2e not in l1e:
-                        lr.append(l1e + [l2e])
-            return lr
-        l1 = [[n] for n in nums]
-        for _ in range(len(nums) -1):
-            l1 = rec(l1, nums)
-        return l1
+        res = []
+        def solve(i,temp):
+            if i >= len(nums):
+                res.append(temp.copy())
+                return
+            for j in range(len(nums)):
+                if nums[j] not in temp:
+                    temp.append(nums[j])
+                    solve(i+1,temp)
+                    temp.pop()
+            # return 
+        solve(0,[])
+        return res
