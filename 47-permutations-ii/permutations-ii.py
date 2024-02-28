c7ce97f9cbd380e7b599f3d2dfc7d7f1c752ad41
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-02-28 13:02:27 +0530

    Time: 57 ms (51.25%) | Memory: 16.9 MB (53.46%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -15,48 +15,48 @@
 #         ret = [list(k) for k in ret]
 #         return ret
 
-class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        p = []
-        v = [False] * len(nums)
-        def backtrack():
-            if len(p) == len(nums):
-                ans.append(p.copy())
-                return
-            used = set()
-            for i in range(len(nums)):
-                if not v[i] and nums[i] not in used:
-                    v[i] = True
-                    p.append(nums[i])
-                    used.add(nums[i])
-                    backtrack()
-                    v[i] = False
-                    p.pop()
-        backtrack()
-        return ans
-
 # class Solution:
 #     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-#         countrecd = {}
-#         for i in nums:
-#             if i in countrecd.keys():
-#                 countrecd[i] += 1
-#             else:
-#                 countrecd[i] = 1
-#         rec = []
-
-#         def solve(arr):
-#             if len(arr) == len(nums):
-#                 print(arr)
-#                 rec.append(arr.copy())
+#         ans = []
+#         p = []
+#         v = [False] * len(nums)
+#         def backtrack():
+#             if len(p) == len(nums):
+#                 ans.append(p.copy())
 #                 return
-#             for num in countrecd.keys():
-#                 if countrecd[num] > 0:
-#                     countrecd[num] -= 1
-#                     arr.append(num)
-#                     solve(arr)
-#                     arr.pop()
-#                     countrecd[num] += 1
-#         solve([])
-#         return rec
+#             used = set()
+#             for i in range(len(nums)):
+#                 if not v[i] and nums[i] not in used:
+#                     v[i] = True
+#                     p.append(nums[i])
+#                     used.add(nums[i])
+#                     backtrack()
+#                     v[i] = False
+#                     p.pop()
+#         backtrack()
+#         return ans
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        countrecd = {}
+        for i in nums:
+            if i in countrecd.keys():
+                countrecd[i] += 1
+            else:
+                countrecd[i] = 1
+        rec = []
+
+        def solve(arr):
+            if len(arr) == len(nums):
+                print(arr)
+                rec.append(arr.copy())
+                return
+            for num in countrecd.keys():
+                if countrecd[num] > 0:
+                    countrecd[num] -= 1
+                    arr.append(num)
+                    solve(arr)
+                    arr.pop()
+                    countrecd[num] += 1
+        solve([])
+        return rec
