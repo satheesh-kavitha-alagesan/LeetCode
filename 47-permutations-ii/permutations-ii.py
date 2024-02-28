ece36020080871e68f6c06c0aa069428fdba0fe2
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-02-28 12:37:48 +0530

    Time: 52 ms (70.99%) | Memory: 16.9 MB (69.49%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,16 +1,59 @@
+# class Solution:
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+#         copy = [ i for i in range(0, len(nums))]
+#         ret = set()
+#         def solve(arr):
+#             if len(arr) == len(copy):
+#                 ret.update([tuple(nums[j] for j in arr)])
+#                 return
+#             for i in range(0, len(copy)):
+#                 if not (i in arr):
+#                     arr.append(copy[i])
+#                     solve(arr)
+#                     arr.pop()
+#         solve([])
+#         ret = [list(k) for k in ret]
+#         return ret
+
+# class Solution:
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+#         ret = []
+#         mapper = [False]*len(nums)
+#         arr = []
+#         def solve():
+#             if len(arr) == len(nums):
+#                 ret.append(arr[:])
+#                 return
+#             used = set()
+#             for i in range(len(nums)):
+#                 if not mapper[i] and nums[i] not in used:
+#                     mapper[i] == True
+#                     arr.append(nums[i])
+#                     used.add(nums[i])
+#                     solve()
+#                     mapper[i] = False
+#                     arr.pop()
+#         print(ret)
+#         solve()
+#         return ret
+
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        copy = [ i for i in range(0, len(nums))]
-        ret = set()
-        def solve(arr):
-            if len(arr) == len(copy):
-                ret.update([tuple(nums[j] for j in arr)])
+        ans = []
+        p = []
+        v = [False] * len(nums)
+        def backtrack():
+            if len(p) == len(nums):
+                ans.append(p[:])
                 return
-            for i in range(0, len(copy)):
-                if not (i in arr):
-                    arr.append(copy[i])
-                    solve(arr)
-                    arr.pop()
-        solve([])
-        ret = [list(k) for k in ret]
-        return ret
+            used = set()
+            for i in range(len(nums)):
+                if not v[i] and nums[i] not in used:
+                    v[i] = True
+                    p.append(nums[i])
+                    used.add(nums[i])
+                    backtrack()
+                    v[i] = False
+                    p.pop()
+        backtrack()
+        return ans
