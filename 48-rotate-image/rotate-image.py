3099797ac595da5b3f4f5bff567b12eef08fb3bb
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-02-29 11:46:22 +0530

    Time: 46 ms (16.16%) | Memory: 16.6 MB (45.98%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,5 +3,12 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        for idx, arr in enumerate(zip(*matrix)):
-            matrix[idx] = arr[::-1]
+        # for idx, arr in enumerate(zip(*matrix)):
+        #     matrix[idx] = arr[::-1]
+
+        for rind in range(len(matrix)):
+            for cind in range(len(matrix)):
+                if cind < rind:
+                    matrix[rind][cind], matrix[cind][rind] = matrix[cind][rind], matrix[rind][cind]
+        for rind in range(len(matrix)):
+            matrix[rind] = matrix[rind][::-1]
