cf881663f7ed835392f46070dd4c6e301eb71a15
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-01 13:28:26 +0530

    Time: 891 ms (13.36%) | Memory: 16.7 MB (46.01%) - LeetSync

---
+++ 18-4sum/4sum.py
@@ -0,0 +1,46 @@
+# class Solution:
+#     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+#         ret = []
+#         if len(nums) < 4:
+#             return ret
+#         nums.sort()
+#         start = 0 
+#         end = len(nums) -1
+#         rotation = [(1,0), (-1,-1), (1,0)]
+#         while start < end:
+#             p1 = start+1
+#             p2 = end -1
+#             twopointtarget = target - (nums[start] + nums[end])
+#             while p1 < p2:
+#                 print(f'{p1 =}, {p2 = }, {twopointtarget = }, {nums[start] = }, {nums[end] = }, {nums[p1] = }, {nums[p2] = }')
+#                 if nums[p1] + nums[p2] == twopointtarget:
+#                     ret.append([nums[start], nums[p1], nums[p2], nums [end]])
+#                     p1 +=1
+#                     p2 -=1
+#                 elif twopointtarget > p1 and twopointtarget <= p2:
+#                     p1+=1
+#                 elif twopointtarget < p2 and twopointtarget >= p1:
+#                     p2-=1
+#                 else:
+#                     break
+#             out = rotation.pop(0)
+#             rotation.append(out)
+#             startchange, endchange = out
+#             start += startchange
+#             end += endchange
+#         return ret
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
+        seen = set()
+        ans = set()
+        for i in range(n):
+            for j in range(i+1, n):
+                for k in range(j+1, n):
+                    lastNumber = target - nums[i] - nums[j] - nums[k]
+                    if lastNumber in seen:
+                        arr = sorted([nums[i], nums[j], nums[k], lastNumber])
+                        ans.add((arr[0], arr[1], arr[2], arr[3]))
+            seen.add(nums[i])
+        return ans
