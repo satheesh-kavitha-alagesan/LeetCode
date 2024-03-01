ceba71e0cee4ffdf698ff4163b9cfaa6b0b70087
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-01 17:15:05 +0530

    Time: 46 ms (90.38%) | Memory: 16.6 MB (56.04%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -21,21 +21,21 @@
 #         print(ret)
 #         return ret
 
-class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        candidates.sort()
-        def dfs(idx, path, cur):
-            if cur > target: return
-            if cur == target:
-                res.append(path)
-                return
-            for i in range(idx, len(candidates)):
-                if i > idx and candidates[i] == candidates[i-1]:
-                    continue
-                dfs(i+1, path+[candidates[i]], cur+candidates[i])
-        dfs(0, [], 0)
-        return res
+# class Solution:
+#     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+#         res = []
+#         candidates.sort()
+#         def dfs(idx, path, cur):
+#             if cur > target: return
+#             if cur == target:
+#                 res.append(path)
+#                 return
+#             for i in range(idx, len(candidates)):
+#                 if i > idx and candidates[i] == candidates[i-1]:
+#                     continue
+#                 dfs(i+1, path+[candidates[i]], cur+candidates[i])
+#         dfs(0, [], 0)
+#         return res
 
 # class Solution:
 #     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
@@ -55,3 +55,31 @@ class Solution:
 #             f(i+1,target)
 #         f(0,target)
 #         return [list(combination) for combination in ans]
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        temp = []
+        ans = []
+        n = len(candidates)
+        def worker(idx,sumi):    
+            if sumi==0:
+                ans.append(temp[:])
+                return
+                
+            if idx == n:
+                return
+
+            for i in range(idx,n):
+                if i!=idx and candidates[i] == candidates[i-1]:
+                    continue
+                if candidates[i]>sumi:
+                    break
+                temp.append(candidates[i])
+                worker(i+1,sumi-candidates[i])
+                temp.remove(candidates[i])
+
+        worker(0,target)
+        return ans                        
+
+        
