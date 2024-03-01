f425a683e49cd6a469fac73cd6cbabfd8ef7e1eb
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-01 13:32:16 +0530

    Time: 136 ms (79.67%) | Memory: 29.1 MB (5.34%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -45,35 +45,52 @@
 #             seen.add(nums[i])
 #         return ans
 
-class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        sortedElementList = sorted(nums)
-        result = []
-        self.nSumRecursive(sortedElementList, target, 4, result, [])
-        return result
+# class Solution:
+#     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+#         sortedElementList = sorted(nums)
+#         result = []
+#         self.nSumRecursive(sortedElementList, target, 4, result, [])
+#         return result
         
-    def twoSum (self, listOfElements: list[int], target: int, resultList: list[list[int]], auxiliaryList: list[int], left: int) -> list[list[int]]:
-        sortedList = sorted(listOfElements)
-        lenght = len(sortedList)
-        right = lenght - 1
-        while (left < right):
-            if (sortedList[left] + sortedList[right] < target): 
-                left+=1
-            elif (sortedList[left] + sortedList[right] > target):
-                right -= 1
-            else:
-                resultList.append(auxiliaryList + [sortedList[left], sortedList[right]])
-                left+=1
-                while(left < right and sortedList[left] == sortedList[left - 1]): left += 1
+#     def twoSum (self, listOfElements: list[int], target: int, resultList: list[list[int]], auxiliaryList: list[int], left: int) -> list[list[int]]:
+#         sortedList = sorted(listOfElements)
+#         lenght = len(sortedList)
+#         right = lenght - 1
+#         while (left < right):
+#             if (sortedList[left] + sortedList[right] < target): 
+#                 left+=1
+#             elif (sortedList[left] + sortedList[right] > target):
+#                 right -= 1
+#             else:
+#                 resultList.append(auxiliaryList + [sortedList[left], sortedList[right]])
+#                 left+=1
+#                 while(left < right and sortedList[left] == sortedList[left - 1]): left += 1
 
-        return
+#         return
 
-    def nSumRecursive (self, listOfElements: list[int], target: int, nSum: int, resultList: list[list[int]], auxiliaryList: list[int], left: int = 0) -> list[list[int]]:
-        if (nSum == 2):
-            self.twoSum(listOfElements, target, resultList, auxiliaryList, left)
-        else:
-            indexLimit = len(listOfElements) - nSum + 1
-            for index in range(left, indexLimit):
-                if (index == left or (listOfElements[index] != listOfElements[index - 1])):
-                    self.nSumRecursive(listOfElements, target - listOfElements[index], nSum - 1, resultList, auxiliaryList + [listOfElements[index]], index + 1)
-        return
+#     def nSumRecursive (self, listOfElements: list[int], target: int, nSum: int, resultList: list[list[int]], auxiliaryList: list[int], left: int = 0) -> list[list[int]]:
+#         if (nSum == 2):
+#             self.twoSum(listOfElements, target, resultList, auxiliaryList, left)
+#         else:
+#             indexLimit = len(listOfElements) - nSum + 1
+#             for index in range(left, indexLimit):
+#                 if (index == left or (listOfElements[index] != listOfElements[index - 1])):
+#                     self.nSumRecursive(listOfElements, target - listOfElements[index], nSum - 1, resultList, auxiliaryList + [listOfElements[index]], index + 1)
+#         return
+
+class Solution:
+    def fourSum(self, A, T):
+        n = len(A)
+        d = defaultdict(set)
+        ans = set()
+        for i in range(n):
+            for j in range(i + 1, n):
+                currentSum = A[i] + A[j]
+                for pair in d[T - currentSum]:
+                    ans.add(tuple(sorted((A[i], A[j]) + pair)))
+            for k in range(i):
+                currentSum = A[k] + A[i]
+                d[currentSum].add((A[k], A[i]))
+        print(ans)
+        return ans
+        
