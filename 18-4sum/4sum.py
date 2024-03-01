c17ded9befe437b2fc91ef3d735d668108a538fe
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-01 13:30:07 +0530

    Time: 479 ms (41.36%) | Memory: 16.6 MB (92.01%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -30,17 +30,50 @@
 #             end += endchange
 #         return ret
 
+# class Solution:
+#     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+#         n = len(nums)
+#         seen = set()
+#         ans = set()
+#         for i in range(n):
+#             for j in range(i+1, n):
+#                 for k in range(j+1, n):
+#                     lastNumber = target - nums[i] - nums[j] - nums[k]
+#                     if lastNumber in seen:
+#                         arr = sorted([nums[i], nums[j], nums[k], lastNumber])
+#                         ans.add((arr[0], arr[1], arr[2], arr[3]))
+#             seen.add(nums[i])
+#         return ans
+
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
-        seen = set()
-        ans = set()
-        for i in range(n):
-            for j in range(i+1, n):
-                for k in range(j+1, n):
-                    lastNumber = target - nums[i] - nums[j] - nums[k]
-                    if lastNumber in seen:
-                        arr = sorted([nums[i], nums[j], nums[k], lastNumber])
-                        ans.add((arr[0], arr[1], arr[2], arr[3]))
-            seen.add(nums[i])
-        return ans
+        sortedElementList = sorted(nums)
+        result = []
+        self.nSumRecursive(sortedElementList, target, 4, result, [])
+        return result
+        
+    def twoSum (self, listOfElements: list[int], target: int, resultList: list[list[int]], auxiliaryList: list[int], left: int) -> list[list[int]]:
+        sortedList = sorted(listOfElements)
+        lenght = len(sortedList)
+        right = lenght - 1
+        while (left < right):
+            if (sortedList[left] + sortedList[right] < target): 
+                left+=1
+            elif (sortedList[left] + sortedList[right] > target):
+                right -= 1
+            else:
+                resultList.append(auxiliaryList + [sortedList[left], sortedList[right]])
+                left+=1
+                while(left < right and sortedList[left] == sortedList[left - 1]): left += 1
+
+        return
+
+    def nSumRecursive (self, listOfElements: list[int], target: int, nSum: int, resultList: list[list[int]], auxiliaryList: list[int], left: int = 0) -> list[list[int]]:
+        if (nSum == 2):
+            self.twoSum(listOfElements, target, resultList, auxiliaryList, left)
+        else:
+            indexLimit = len(listOfElements) - nSum + 1
+            for index in range(left, indexLimit):
+                if (index == left or (listOfElements[index] != listOfElements[index - 1])):
+                    self.nSumRecursive(listOfElements, target - listOfElements[index], nSum - 1, resultList, auxiliaryList + [listOfElements[index]], index + 1)
+        return
