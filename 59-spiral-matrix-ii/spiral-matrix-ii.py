e4857fc1f37402d3d317e9756f972820959cc8a3
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-05 11:36:46 +0530

    Time: 35 ms (65.77%) | Memory: 16.7 MB (29.99%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.py
@@ -0,0 +1,54 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        dp = [[0]*n for _ in range(n)]
+        startind = [0,0]
+        count = 1
+        cur = 'l_to_r'
+        row, col = (0,0)
+        while count <= n*n:
+            print(row,col)
+            dp[row][col] = count
+            count +=1
+            if cur == 'l_to_r':
+                if col +1 < len(dp[col]):
+                    if dp[row][col +1] == 0:
+                        col = col+1
+                    else:
+                        cur = 't_to_b'
+                        row = row+1
+                else:
+                    cur = 't_to_b'
+                    row = row+1
+            elif cur == 't_to_b':
+                if row +1 < len(dp):
+                    if dp[row+1][col] == 0:
+                        row = row+1
+                    else:
+                        cur = 'r_to_l'
+                        col = col -1
+                else:
+                    cur = 'r_to_l'
+                    col = col -1
+            elif cur == 'r_to_l':
+                if col -1 >= 0:
+                    if dp[row][col -1] ==0:
+                        col = col-1
+                    else:
+                        cur = 'b_to_t'
+                        row = row -1
+                else:
+                    cur = 'b_to_t'
+                    row = row -1
+            elif cur == 'b_to_t':
+                if row -1 >=0:
+                    if dp[row -1][col] == 0:
+                        row = row -1
+                    else:
+                        cur = 'l_to_r'
+                        col = col +1
+                else:
+                    cur = 'l_to_r'
+                    col = col +1
+
+        print(dp)
+        return dp
