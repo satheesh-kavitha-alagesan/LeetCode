bcd9ac7110536008e922c37d7b18576fde85081c
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-05 11:38:55 +0530

    Time: 33 ms (78.88%) | Memory: 16.7 MB (29.99%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/spiral-matrix-ii.py
@@ -13,42 +13,30 @@ class Solution:
                 if col +1 < len(dp[col]):
                     if dp[row][col +1] == 0:
                         col = col+1
-                    else:
-                        cur = 't_to_b'
-                        row = row+1
-                else:
-                    cur = 't_to_b'
-                    row = row+1
+                        continue
+                cur = 't_to_b'
+                row = row+1
             elif cur == 't_to_b':
                 if row +1 < len(dp):
                     if dp[row+1][col] == 0:
                         row = row+1
-                    else:
-                        cur = 'r_to_l'
-                        col = col -1
-                else:
-                    cur = 'r_to_l'
-                    col = col -1
+                        continue
+                cur = 'r_to_l'
+                col = col -1
             elif cur == 'r_to_l':
                 if col -1 >= 0:
                     if dp[row][col -1] ==0:
                         col = col-1
-                    else:
-                        cur = 'b_to_t'
-                        row = row -1
-                else:
-                    cur = 'b_to_t'
-                    row = row -1
+                        continue
+                cur = 'b_to_t'
+                row = row -1
             elif cur == 'b_to_t':
                 if row -1 >=0:
                     if dp[row -1][col] == 0:
                         row = row -1
-                    else:
-                        cur = 'l_to_r'
-                        col = col +1
-                else:
-                    cur = 'l_to_r'
-                    col = col +1
+                        continue
+                cur = 'l_to_r'
+                col = col +1
 
         print(dp)
         return dp
