8ab63ac757d9688ef1ba521dcd5c6d72bbfe1f59
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-07 10:57:43 +0530

    Time: 103 ms (57.12%) | Memory: 17.5 MB (82.71%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,23 +1,39 @@
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        changed = set()
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if matrix[r][c] == 0 and (r,c) not in changed:
-                    self.change(matrix, r,c, changed) 
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         changed = set()
+#         for r in range(len(matrix)):
+#             for c in range(len(matrix[0])):
+#                 if matrix[r][c] == 0 and (r,c) not in changed:
+#                     self.change(matrix, r,c, changed) 
 
             
-    def change(self, matrix, r, c, changed):
+#     def change(self, matrix, r, c, changed):
         
-        for cr in range(0, len(matrix)):
-            if matrix[cr][c] != 0:
-                matrix[cr][c] = 0
-                changed.add((cr,c))
+#         for cr in range(0, len(matrix)):
+#             if matrix[cr][c] != 0:
+#                 matrix[cr][c] = 0
+#                 changed.add((cr,c))
 
-        for cc in range(0, len(matrix[0])):
-            if matrix[r][cc] != 0:
-                matrix[r][cc] = 0
-                changed.add((r,cc))
+#         for cc in range(0, len(matrix[0])):
+#             if matrix[r][cc] != 0:
+#                 matrix[r][cc] = 0
+#                 changed.add((r,cc))
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows=len(matrix)
+        cols=len(matrix[0])
+        temp=[]
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j]==0:
+                    temp.append((i,j))
+        for p,q in temp:
+            matrix[p]=[0]*cols
+            for r in range(rows):
+                matrix[r][q]=0            
+
+        
