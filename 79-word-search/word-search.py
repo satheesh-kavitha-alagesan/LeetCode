df00d03ecef67c351e3251613cb7cbdc24bc9f98
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-08 11:13:05 +0530

    Time: 4306 ms (46.73%) | Memory: 16.5 MB (77.22%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,3 +1,4 @@
+## Solution 1 False
 # class Solution:
 #     def exist(self, board: List[List[str]], word: str) -> bool:
 #             global flag
@@ -28,41 +29,80 @@
 #                             else:
 #                                 return True
 #             return False
+
+# #Solution2
+# class Solution:
+#     def exist(self, board: List[List[str]], word: str) -> bool:
+#         boardDic = defaultdict(int)
+#         for i in range(len(board)):
+#             for j in range(len(board[0])):
+#                 boardDic[board[i][j]] += 1
+
+#         wordDic = Counter(word)
+#         for c in wordDic:
+#             if c not in boardDic or boardDic[c] < wordDic[c]:
+#                 return False
+
+#         for i in range(len(board)):
+#             for j in range(len(board[0])):
+#                 if board[i][j] == word[0]:
+#                     if self.dfs(i, j, 0, board, word):
+#                         return True
+
+#         return False
+
+#     def dfs(self, i, j, k, board, word):
+#         if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or \
+#         k >= len(word) or word[k] != board[i][j]:
+#             return False
+
+#         if k == len(word) - 1:
+#             return True
+
+#         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+#         for x, y in directions:
+#             tmp = board[i][j]
+#             board[i][j] = -1
+
+#             if self.dfs(i + x, j + y, k + 1, board, word): 
+#                 return True
+
+#             board[i][j] = tmp
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        boardDic = defaultdict(int)
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                boardDic[board[i][j]] += 1
-
-        wordDic = Counter(word)
-        for c in wordDic:
-            if c not in boardDic or boardDic[c] < wordDic[c]:
-                return False
-
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    if self.dfs(i, j, 0, board, word):
-                        return True
-
-        return False
-
-    def dfs(self, i, j, k, board, word):
-        if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or \
-        k >= len(word) or word[k] != board[i][j]:
-            return False
-
-        if k == len(word) - 1:
-            return True
-
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        for x, y in directions:
-            tmp = board[i][j]
-            board[i][j] = -1
-
-            if self.dfs(i + x, j + y, k + 1, board, word): 
-                return True
-
-            board[i][j] = tmp
+            global flag
+            flag = False
+            m= len(board)
+            n= len(board[0])
+            def rec(ind, word, bd):
+                global flag
+                r, c = ind
+                if (word == '') or (flag == True):
+                    flag = True
+                    return
+                elif bd[r][c] == word[0]:
+                    tmp = bd[r][c]
+                    bd[r][c] = -1
+                    if c+1<n:
+                        rec((r,c+1), word[1::], bd)
+                    if c-1 >=0:
+                        rec((r,c-1), word[1::], bd)
+                    if r+1<m:
+                        rec((r+1,c), word[1::], bd)
+                    if r-1 >=0:
+                        rec((r-1,c), word[1::], bd)
+                    if word[1::] == '':
+                        flag = True
+                        return
+                    bd[r][c] = tmp
+                else:
+                    return
+            for i in range(m):
+                    for j in range(n):
+                            if not flag:
+                                    rec((i,j), word, board)
+                            else:
+                                return True
+            return flag
