a9255abb4097389afb984b3700404f16c31f7977
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-09 21:16:54 +0530

    Time: 39 ms (63.01%) | Memory: 16.5 MB (53.55%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,6 +1,23 @@
+# #Soution 1
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         ret = 1
+#         while ret*ret <= x:
+#             ret +=1
+#         return ret -1
+
+#Solution 2
 class Solution:
     def mySqrt(self, x: int) -> int:
-        ret = 1
-        while ret*ret <= x:
-            ret +=1
-        return ret -1
+        l = 0
+        r = x
+
+        while l < r:
+            mid = l + (r - l + 1) // 2
+
+            if mid ** 2 <= x:
+                l = mid
+            else:
+                r = mid - 1
+
+        return l
