5f423ef4288f0dcb90fc3ede42211960f13dfe51
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-10 20:06:33 +0530

    Time: 44 ms (5.15%) | Memory: 16.5 MB (38.67%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -4,14 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# #Solution 1
+# class Solution:
+#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         ret = []
+#         def rec(nd):
+#             if nd is None:
+#                 return
+#             rec(nd.left)
+#             ret.append(nd.val)
+#             rec(nd.right)
+#         rec(root)
+#         return ret
+
+#Solution 2
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ret = []
-        def rec(nd):
-            if nd is None:
-                return
-            rec(nd.left)
-            ret.append(nd.val)
-            rec(nd.right)
-        rec(root)
-        return ret
+        res =[]
+        stack = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            cur = stack.pop()
+            res.append(cur.val)
+            root = cur.right
