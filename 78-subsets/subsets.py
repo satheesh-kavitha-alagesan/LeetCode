c1a133099ce379077e1248498aa67fac6981d202
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-10 17:26:04 +0530

    Time: 36 ms (64.26%) | Memory: 16.8 MB (42.39%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,14 +1,30 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         ret = set()
+#         temp = []
+#         def dfs(ind):
+#             ret.add(tuple(temp))
+#             if ind >= len(nums):
+#                 return
+#             temp.append(nums[ind])
+#             dfs(ind+1)
+#             temp.pop()
+#             dfs(ind+1)
+#         dfs(0)
+#         return ret
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ret = set()
-        temp = []
-        def dfs(ind):
-            ret.add(tuple(temp))
-            if ind >= len(nums):
-                return
-            temp.append(nums[ind])
-            dfs(ind+1)
-            temp.pop()
-            dfs(ind+1)
-        dfs(0)
-        return ret
+        answer = []
+        current = []
+        n = len(nums)
+        def back(index):
+            answer.append(current[:])
+            for i in range(index,n):
+                current.append(nums[i])
+                back(i+1)
+                current.pop()
+
+        back(0)
+        return answer
+        
