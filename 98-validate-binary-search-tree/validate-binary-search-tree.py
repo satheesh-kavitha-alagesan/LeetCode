ed043de9feb4baa3f525e9aa34e9583372400853
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-11 12:03:09 +0530

    Time: 33 ms (94.15%) | Memory: 18.1 MB (97.60%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#         def validate(node, valrange = (float('-inf'), float('inf'))):
+#             if node.left:
+#                 if valrange[0] < node.left.val < node.val < valrange[1]:
+#                     return validate(node.left, (valrange[0], node.val))
+#                 else:
+#                     return False
+#             if node.right:
+#                 if valrange[0] < node.val < node.right.val < valrange[1]:
+#                     return validate(node.right, (node.val, valrange[1]))
+#                 else:
+#                     return False
+#             left = []
+#             return True
+#         return validate(root)
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        prev = float('-inf')
+        def inorder(node):
+            nonlocal prev
+            if not node:
+                return True
+            if not (inorder(node.left) and prev < node.val):
+                return False
+            prev = node.val
+            return inorder(node.right)
+        return inorder(root)
