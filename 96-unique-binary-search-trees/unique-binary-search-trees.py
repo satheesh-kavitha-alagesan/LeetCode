882d03c5e5a8440dd7df35cbccce33f0c45442fa
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-13 12:12:43 +0530

    Time: 37 ms (42.31%) | Memory: 16.6 MB (40.44%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,12 +1,9 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         dp = [1]*(n+1)
-
         for node in range(2, n+1):
             total = 0
             for root in range(1,node+1):
-                left = root -1
-                right = node - root
-                total += dp[left] * dp[right]
+                total += dp[root -1] * dp[node - root]
             dp[node] = total
         return dp[n]
