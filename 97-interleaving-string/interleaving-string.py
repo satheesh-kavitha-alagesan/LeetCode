9a89f4166a1a950fcee20cb9161509ed6c742bdb
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-19 09:55:53 +0530

    Time: 31 ms (96.92%) | Memory: 16.5 MB (95.61%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,25 +1,47 @@
-import numpy as np
+# import numpy as np
+# class Solution:
+#     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+#         m, n, l = len(s1), len(s2), len(s3)
+#         if m + n != l:
+#             return False
+        
+#         dp = [[False] * (n + 1) for _ in range(m + 1)]
+#         dp[0][0] = True
+        
+#         for i in range(1, m + 1):
+#             dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
+        
+#         for j in range(1, n + 1):
+#             dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+        
+#         for i in range(1, m + 1):
+#             for j in range(1, n + 1):
+#                 dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+        
+#         for ind, d in enumerate(dp):
+#             print(ind, np.array(d))
+#             print('-------------')
+
+#         return dp[m][n]
+
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         m, n, l = len(s1), len(s2), len(s3)
         if m + n != l:
             return False
         
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
+        if m < n:
+            return self.isInterleave(s2, s1, s3)
         
-        for i in range(1, m + 1):
-            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
+        dp = [False] * (n + 1)
+        dp[0] = True
         
         for j in range(1, n + 1):
-            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+            dp[j] = dp[j-1] and s2[j-1] == s3[j-1]
         
         for i in range(1, m + 1):
+            dp[0] = dp[0] and s1[i-1] == s3[i-1]
             for j in range(1, n + 1):
-                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+                dp[j] = (dp[j] and s1[i-1] == s3[i+j-1]) or (dp[j-1] and s2[j-1] == s3[i+j-1])
         
-        for d in dp:
-            print(np.array(dp))
-            print('-------------')
-
-        return dp[m][n]
+        return dp[n]
