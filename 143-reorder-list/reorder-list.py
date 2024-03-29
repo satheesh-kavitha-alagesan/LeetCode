ba5e1556415258be962cceed9f4ebf4a560d0f95
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-29 12:43:07 +0530

    Time: 60 ms (30.60%) | Memory: 24.6 MB (86.44%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -3,27 +3,48 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# #Solution1
+# class Solution:
+#     def reorderList(self, head: Optional[ListNode]) -> None:
+#         """
+#         Do not return anything, modify head in-place instead.
+#         """
+#         dummy = slow = fast = head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+        
+#         st = slow.next
+#         slow.next = None
+#         stack = []
+#         while st:
+#             stack.append(st.val)
+#             st = st.next
+
+#         while dummy:
+#             temp = dummy.next
+#             if stack:
+#                 dummy.next = ListNode(stack.pop())
+#                 dummy.next.next = temp
+#             dummy = temp
+#         return head
+
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        dummy = slow = fast = head
+    def reorderList(self, head: ListNode) -> None:
+        if not head:
+            return 
+        
+        slow = fast = head
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next
-        
-        st = slow.next
-        slow.next = None
-        stack = []
-        while st:
-            stack.append(st.val)
-            st = st.next
+            fast = fast.next.next 
+            
+        prev, curr = None, slow
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next       
 
-        while dummy:
-            temp = dummy.next
-            if stack:
-                dummy.next = ListNode(stack.pop())
-                dummy.next.next = temp
-            dummy = temp
-        return head
+        first, second = head, prev
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
