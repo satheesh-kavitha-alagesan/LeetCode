0d1c503a4629128cdfa25bef328b3a7c7db38cc1
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-03-29 11:53:22 +0530

    Time: 49 ms (22.83%) | Memory: 19.1 MB (44.09%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -4,13 +4,35 @@
 #         self.val = x
 #         self.next = None
 
+# #Solution1
+# class Solution:
+#     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         idvault = {}
+#         inc = 0
+#         while head:
+#             if id(head) in idvault:
+#                 return idvault[id(head)]
+#             else:
+#                 idvault[id(head)] = head
+#             head = head.next
+
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        idvault = {}
-        inc = 0
-        while head:
-            if id(head) in idvault:
-                return idvault[id(head)]
-            else:
-                idvault[id(head)] = head
-            head = head.next
+        if not head:
+            return None
+        
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        
+        if not fast or not fast.next:
+            return None
+
+        slow2 = head
+        while slow2 != slow:
+            slow = slow.next
+            slow2 = slow2.next
+        return slow
