ac60c7c5105c5d1eef412ca32a854016135db47e
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-04-15 20:30:01 +0530

    Time: 301 ms (37.11%) | Memory: 16.5 MB (97.67%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -38,37 +38,70 @@
 #             if val in grid[gridmap]:
 #                 grid[gridmap].remove(val)
             
+# class Solution:
+#     def solveSudoku(self, board: List[List[str]]) -> None:
+#         n = 9
+#         def isValid(row, col, ch):
+#             row, col = int(row), int(col)
+#             for i in range(9):
+#                 if board[i][col] == ch:
+#                     return False
+#                 if board[row][i] == ch:
+#                     return False
+                
+#                 if board[3*(row//3) + i//3][3*(col//3) + i%3] == ch:
+#                     return False
+#             return True  
+#         def solve(row, col):
+#             if row == n:
+#                 return True
+#             if col == n:
+#                 return solve(row+1, 0)
+            
+#             if board[row][col] == ".":
+#                 for i in range(1, 10):
+#                     if isValid(row, col, str(i)):
+#                         board[row][col] = str(i)
+                        
+#                         if solve(row, col + 1):
+#                             return True
+#                         else:
+#                             board[row][col] = "."
+#                 return False
+#             else:
+#                 return solve(row, col + 1)
+#         solve(0, 0)
+		
+
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         n = 9
-        def isValid(row, col, ch):
-            row, col = int(row), int(col)
-            for i in range(9):
-                if board[i][col] == ch:
+        def isValid(nrow, ncol, ch):
+            nrow, ncol = int(nrow), int(ncol)
+            for num in range(9):
+                if board[num][ncol] == ch:
                     return False
-                if board[row][i] == ch:
+                if board[nrow][num] == ch:
                     return False
-                
-                if board[3*(row//3) + i//3][3*(col//3) + i%3] == ch:
+                if board[3*(nrow//3)+(num//3)][3*(ncol//3)+(num%3)] == ch:
                     return False
-            return True  
+            return True
+        
         def solve(row, col):
             if row == n:
                 return True
             if col == n:
                 return solve(row+1, 0)
-            
-            if board[row][col] == ".":
-                for i in range(1, 10):
+            if board[row][col] == '.':
+                for i in range(1,10):
                     if isValid(row, col, str(i)):
                         board[row][col] = str(i)
-                        
-                        if solve(row, col + 1):
+                        if solve(row, col+1):
                             return True
                         else:
-                            board[row][col] = "."
+                            board[row][col] = '.'
                 return False
             else:
-                return solve(row, col + 1)
-        solve(0, 0)
-		
+                return solve(row, col+1)
+        solve(0,0)
+        return board
