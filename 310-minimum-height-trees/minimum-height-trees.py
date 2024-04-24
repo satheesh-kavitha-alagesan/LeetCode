4d8c0291e3893f3c10ca34c96eff49332a15d24b
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-04-24 12:06:20 +0530

    Time: 1269 ms (11.61%) | Memory: 40.3 MB (5.10%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -37,37 +37,75 @@
 #                 ret.append(key)
 #         return ret
 
+# Solution 2
+# class Solution:
+#     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+#         counts = [0] * n
+#         links = [0] * n
+        
+#         for edge in edges:
+#             links[edge[0]] ^= edge[1]
+#             counts[edge[0]] += 1
+#             links[edge[1]] ^= edge[0]
+#             counts[edge[1]] += 1
+        
+#         Qu = deque()
+#         dists = [0] * n
+        
+#         for i in range(n):
+#             if counts[i] == 1:
+#                 Qu.append(i)
+        
+#         stp = 1
+#         while Qu:
+#             size = len(Qu)
+#             for _ in range(size):
+#                 tmp = Qu.popleft()
+#                 links[links[tmp]] ^= tmp
+#                 counts[links[tmp]] -= 1
+#                 if counts[links[tmp]] == 1:
+#                     dists[links[tmp]] = max(stp, dists[links[tmp]])
+#                     Qu.append(links[tmp])
+#             stp += 1
+        
+#         max_dist = max(dists)
+#         res = [i for i in range(n) if dists[i] == max_dist]
+        
+#         return res
+
+class Node:
+    def __init__(self, val: int):
+        self.val: int = val
+        self.connected: list = []
+
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        counts = [0] * n
-        links = [0] * n
+        graph = {i: Node(i) for i in range(n)}
+        for val1, val2 in edges:
+            graph[val1].connected.append(graph[val2])
+            graph[val2].connected.append(graph[val1])
         
-        for edge in edges:
-            links[edge[0]] ^= edge[1]
-            counts[edge[0]] += 1
-            links[edge[1]] ^= edge[0]
-            counts[edge[1]] += 1
+        node, path = self.run_dfs(0, graph)
+        node, path = self.run_dfs(node, graph)
         
-        Qu = deque()
-        dists = [0] * n
-        
-        for i in range(n):
-            if counts[i] == 1:
-                Qu.append(i)
-        
-        stp = 1
-        while Qu:
-            size = len(Qu)
-            for _ in range(size):
-                tmp = Qu.popleft()
-                links[links[tmp]] ^= tmp
-                counts[links[tmp]] -= 1
-                if counts[links[tmp]] == 1:
-                    dists[links[tmp]] = max(stp, dists[links[tmp]])
-                    Qu.append(links[tmp])
-            stp += 1
-        
-        max_dist = max(dists)
-        res = [i for i in range(n) if dists[i] == max_dist]
-        
-        return res
+        if len(path) % 2 == 0:
+            return [path[len(path) // 2], path[len(path) // 2 -1]]
+        else:
+            return [path[len(path) // 2]]
+
+    @staticmethod
+    def run_dfs(init_pos, graph):
+
+        stack = [(init_pos, [init_pos])]
+        furthest = init_pos, [init_pos]
+
+        while stack:
+            val, path = stack.pop(-1)
+
+            if len(path) >= len(furthest[1]):
+                furthest = val, path
+            
+            for connection in graph[val].connected:
+                if len(path) <= 1 or path[-2] != connection.val:
+                    stack.append((connection.val, path + [connection.val]))
+        return furthest
