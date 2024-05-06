46f63b13857f467e877bdc3c2efa06f3070c99e5
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-05-06 21:13:13 +0530

    Time: 377 ms (96.27%) | Memory: 51 MB (90.29%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -3,16 +3,44 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Solution 1
+# class Solution:
+#     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         ma = [0]
+#         def reco(nd):
+#             if nd != None:
+#                 ret = reco(nd.next)
+#                 if nd.val >= ma[0]:
+#                     ma[0] = nd.val
+#                     return ListNode(nd.val, ret)
+#                 return ret
+#             else:
+#                 return None
+#         return reco(head)
+
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ma = [0]
-        def reco(nd):
-            if nd != None:
-                ret = reco(nd.next)
-                if nd.val >= ma[0]:
-                    ma[0] = nd.val
-                    return ListNode(nd.val, ret)
-                return ret
-            else:
-                return None
-        return reco(head)
+        #first reverse the list
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        
+        curr = prev
+        while curr:
+            while curr.next and curr.val>curr.next.val:
+                curr.next = curr.next.next
+            curr=curr.next
+
+        curr = prev
+        prev = None
+        while curr:
+            next = curr.next
+            curr.next=prev
+            prev =curr
+            curr = next
+        return prev
