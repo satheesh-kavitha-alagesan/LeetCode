eefafa34010c61931662016b2c335b6705a6df04
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-07-23 07:41:25 +0530

    Time: 103 ms (65.81%) | Memory: 17.1 MB (14.60%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,3 +1,3 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        return [i[0] for i in sorted([(nm, he) for nm, he in zip(names, heights)], key = lambda x: (-x[1], x[0]))]
+        return [i[0] for i in sorted(zip(names, heights), key = lambda x: (-x[1], x[0]))]
