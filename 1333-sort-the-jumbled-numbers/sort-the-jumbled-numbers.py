6da5b9a2902b640db5714bb8e8a78465f22b5f3e
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-07-24 20:31:11 +0530

    Time: 838 ms (92.39%) | Memory: 26.7 MB (18.48%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -2,10 +2,18 @@ class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
         maps = defaultdict(list)
         for num in nums:
-            finalNum = ''
-            for dig in str(num):
-                finalNum += str(mapping[int(dig)])
-            maps[int(finalNum)].append(num)
+            cp = num
+            finalNum = 0
+            mu = 1
+            if num == 0:
+                maps[mapping[0]].append(cp)
+                continue
+            while num > 0:
+                rem = num %10
+                num //= 10
+                finalNum += mapping[rem] * mu
+                mu *=10
+            maps[int(finalNum)].append(cp)
         ret = []
         for _k in sorted(maps.keys()):
             ret.extend(maps[_k])
