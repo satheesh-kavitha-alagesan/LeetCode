079ef3b08243bda82d57c9bda44a1399cfd3b4c9
satheesh-kavitha-alagesan <48002985+satheesh-kavitha-alagesan@users.noreply.github.com>
2024-07-25 08:57:33 +0530

    Time: 1367 ms (14.98%) | Memory: 24.8 MB (48.21%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         def split(li):
@@ -7,12 +9,13 @@ class Solution:
             else:
                 return li, []
         l1, l2 = split(nums)
+        l1, l2 = deque(l1), deque(l2)
         ret = []
         while l1 and l2:
             if l1[0] > l2[0]:
-                ret.append(l2.pop(0))
+                ret.append(l2.popleft())
             else:
-                ret.append(l1.pop(0))
+                ret.append(l1.popleft())
         if l1:
             ret.extend(l1)
         if l2:
